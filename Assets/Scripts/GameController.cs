89a58c55551db0011731302c4e3e43955b03623a
Piotr Masek <piotr3km@gmail.com>
2015-11-05 01:15:06 +0100

    Added Main Menu, and Pause Menu with GameController class
    TODO: Fix multiple instances of GameController after loading Main Menu

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject m_StartGame;
+    public GameObject m_ExitGame;
+    public GameObject m_MainMenu;
+
+    private bool m_IsPaused = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.GetButtonDown("Cancel"))
+        {
+            if(m_IsPaused)
+                UnpauseGame();
+            else
+                PauseGame();
+        }
+
+        if(Application.loadedLevel == 0)
+        {
+            m_StartGame.SetActive(true);
+            m_ExitGame.SetActive(true);
+            m_MainMenu.SetActive(false);
+        }
+        else
+        {
+            m_StartGame.SetActive(false);
+            m_ExitGame.SetActive(false);
+
+            if (m_IsPaused)
+                m_MainMenu.SetActive(true);
+            else
+                m_MainMenu.SetActive(false);
+
+
+        }
+	}
+
+    public void StartGame()
+    {
+        Application.LoadLevel(1);
+        UnpauseGame();
+    }
+
+    public void MainMenu()
+    {
+        Application.LoadLevel(0);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        m_IsPaused = true;
+    }
+
+    public void UnpauseGame()
+    {
+        Time.timeScale = 1;
+        m_IsPaused = false;
+    }
+
+}
