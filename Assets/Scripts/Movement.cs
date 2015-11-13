7b853773707ce8a039728bf409cc0c27babaabe8
Piotr Masek <piotr3km@gmail.com>
2015-11-13 02:58:34 +0100

    Fixed movement direction flipping

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -9,31 +9,37 @@ public class Movement : MonoBehaviour {
     public Animator m_Animator;
 
     public bool grounded = false;
+    public bool facingRight = true;
     
 	// Use this for initialization
-	void Start () {
-	
-	}
-	
+	void Start ()
+    {
+
+    }
+    
+    // Update is called once per frame
     void Update()
     {
 
     }
 
-	// Update is called once per frame
-	void FixedUpdate () {
-
+	void FixedUpdate ()
+    {
         m_Animator.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
 
-        if (Input.GetButton("Horizontal")) {
-            rb.velocity = new Vector2(Input.GetAxis("Horizontal")*speed*Time.fixedDeltaTime, rb.velocity.y);
+        if (Input.GetButton("Horizontal"))
+        {
+            rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed * Time.fixedDeltaTime, rb.velocity.y);
 
-            
-
-            if(Input.GetAxis("Horizontal") >= 0) {
-                transform.eulerAngles = new Vector3(transform.eulerAngles.x, 0, transform.eulerAngles.z);
-            } else {
-                transform.eulerAngles = new Vector3(transform.eulerAngles.x, 180, transform.eulerAngles.z);
+            if(Input.GetAxis("Horizontal") >= 0)
+            {
+                if (!facingRight)
+                    Flip();
+            }
+            else
+            {
+                if (facingRight)
+                    Flip();
             }
         }
 
@@ -42,12 +48,23 @@ public class Movement : MonoBehaviour {
         }
 	}
 
-    void OnTriggerEnter2D(Collider2D other) {
+    //TODO: Fix colliders
+    void OnTriggerEnter2D(Collider2D other)
+    {
         grounded = true;
     }
 
-    void OnTriggerExit2D(Collider2D other) {
+    void OnTriggerExit2D(Collider2D other)
+    {
         grounded = false;
     }
 
+    void Flip()
+    {
+        facingRight = !facingRight;
+
+        Vector2 local_scale = transform.localScale;
+        local_scale.x *= -1;
+        transform.localScale = local_scale;
+    }
 }
